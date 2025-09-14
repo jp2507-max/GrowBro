a03605d32acf545e47ada888fdfa5fcd35d71017
jp2507-max <jan-blohm@gmx.de>
2025-09-14 11:38:55 +0200

    fix(metro): base on expo-router/metro + compose Sentry and NativeWind

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
 const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config');
+const { withExpoRouter } = require('expo-router/metro');
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
-const config = getSentryExpoConfig(__dirname);
+let config = getDefaultConfig(__dirname);
 
+// Required: enable context modules and other Router transforms
+config = withExpoRouter(config);
+
+// Compose Sentry instrumentation on top of the router config
+config = getSentryExpoConfig(__dirname, config);
+
+// NativeWind CSS support
 module.exports = withNativeWind(config, { input: './global.css' });
